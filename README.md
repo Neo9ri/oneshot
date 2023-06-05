a079d503409964db04cd7eea82f29ca8475ad757
Neo9ri <seung15781@naver.com>
2023-06-05 23:18:48 +0900

    프로젝트 초기 설정 및 MySQL 연결

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # oneshot
 라팀의 파이널 프로젝트 &lt;원 샷 : 우리 술 한잔>의 원격 저장소입니다.
-woo 브렌치 테스트 입니다 -백채우
-git 테스트 입니다. -백채우
+<br>
+<br>java version : 11
+<br>db : MySQL 8.0
+<br>db username : root
+<br>db userpassword : 1111
---
+++ src/main/java/himedia/oneshot/OneshotApplication.java
@@ -0,0 +1,13 @@
+package himedia.oneshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneshotApplication.class, args);
+	}
+
+}
