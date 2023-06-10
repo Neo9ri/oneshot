b8647d9d63d24b402800b64aef67452e785f914b
juniejung <jooeunjung0429@gmail.com>
2023-06-11 06:08:42 +0900

    util의 user 이미지 삭제되어있어 새로 이미지 추가 후 header 수정, footer 안닫힌 p태그 발견하여 수정, 폰트 굵기 설정 안되어 reset.css 폰트 링크 수정, JdbcAdminProductRepository 수정 및 JdbcAdminProductRepositoryTest 완료 테스트시 로그 보이도록 dependency 추가, 상품 수정 메서드를 위해 Product 생성자 수정, 중복되는 css 파일 삭제

--- src/test/java/himedia/oneshot/repository/JdbcAdminProductRepositoryTest.java
+++ src/test/java/himedia/oneshot/repository/JdbcAdminProductRepositoryTest.java
@@ -3,12 +3,12 @@ package himedia.oneshot.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import himedia.oneshot.entity.Product;
-import himedia.oneshot.repository.JdbcAdminProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
 
 
 @SpringBootTest
@@ -21,17 +21,35 @@ public class JdbcAdminProductRepositoryTest {
     @Test
     void save() {
         //given
-        Product product = new Product("테스트",1,7000,"img/product/thumbnail/test.jpg");
-//        log.info("저장전 id",product.getId());
-
+        Product beforeSave = new Product("테스트",1,"서울","증류주","테스터",17,7000,"img/product/thumbnail/test.jpg",null,null,null);
+        log.info("id",beforeSave.getId());
         // when
-        Product saveProduct = adminProductRepository.save(product);
-//        log.info("저장후 id",product.getId());
-
+        Product afterSave = adminProductRepository.saveProduct(beforeSave);
+        log.info("id",afterSave.getId());
         // then
-        assertThat(saveProduct.getId()).isEqualTo(product.getId());
+        assertThat(afterSave.getId()).isEqualTo(beforeSave.getId());
+    }
 
+    @Test
+    void update(){
+        //given
+        Product beforeSave = new Product("테스트2",1,"강원,세종권","과실주","테스터",25,24000,"img/product/thumbnail/test2.jpg",null,null,null);
+        Product afterSave = adminProductRepository.saveProduct(beforeSave);
+        //when
+        Product update = new Product("수정테스트",1,"충청, 제주","기타주","수정테스터",13,33000,"img/product/thumbnail/updated.jpg",null,null,null);
+        adminProductRepository.updateProduct(beforeSave.getId(),update);
+        //then
+        assertThat(adminProductRepository.findById(beforeSave.getId()).get().getName()).isEqualTo(update.getName());
+        log.info("afterSave name >> {}",afterSave.getName());
+        log.info("updated name >> {}",update.getName());
+    }
 
+    @Test
+    void delete(){
+        Product beforeSave = new Product("테스트2",1,"강원,세종권","과실주","테스터",25,24000,"img/product/thumbnail/test2.jpg",null,null,null);
+        adminProductRepository.saveProduct(beforeSave);
+        adminProductRepository.deleteProduct(beforeSave.getId());
+        assertThat(adminProductRepository.findById(beforeSave.getId())).isEmpty();
 
     }
 }
