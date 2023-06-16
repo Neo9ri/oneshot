10355a4297936f20a417f5188da385783453fc54
juniejung <jooeunjung0429@gmail.com>
2023-06-16 09:10:52 +0900

    세부사항 확인 필요

--- src/main/java/himedia/oneshot/controller/AdminProductController.java
+++ src/main/java/himedia/oneshot/controller/AdminProductController.java
@@ -6,11 +6,8 @@ import himedia.oneshot.service.LoginService;
 import himedia.oneshot.service.Pagination;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +17,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.*;
 
 @Slf4j
@@ -31,14 +27,14 @@ public class AdminProductController {
     private final AdminProductService adminProductService;
     private final LoginService loginService;
 
-    private List<String> typeLocal = List.of(
+    private final List<String> typeLocal = List.of(
             "서울, 경기, 인천권",
             "강원, 세종권",
             "충북, 충남, 제주도",
             "전북, 전남, 경북, 경남"
     );
 
-    private List<String> typeKind = List.of("증류주", "과실주","약주/청주","숙성 전통주","기타");
+    private final List<String> typeKind = List.of("증류주", "과실주","약주/청주","숙성 전통주","기타");
 
     @GetMapping("/product/add")
     public String addProduct(Model model) {
@@ -74,23 +70,69 @@ public class AdminProductController {
         model.addAttribute("typeLocal", typeLocal);
         model.addAttribute("typeKind", typeKind);
         model.addAttribute("selectedTypeKind", product.getType_kind());
-//        long timestamp = System.currentTimeMillis(); // 현재 타임스탬프를 고유 파라미터로 사용
-//
-//        // 이미지 URL에 고유 파라미터 추가
-//        String thumbImgUrl = "/" + product.getImg_thumb() + "?param=" + timestamp;
-//        String expImgUrl1 = "/" + product.getImg_exp1() + "?param=" + timestamp;
-//        model.addAttribute("thumbImgUrl", thumbImgUrl);
-//        model.addAttribute("expImgUrl1", expImgUrl1);
-//        if (product.getImg_exp2() !=null){
-//            String expImgUrl2 = "/" + product.getImg_exp2() + "?param=" + timestamp;
-//            model.addAttribute("expImgUrl2", expImgUrl2);
-//        }
-//        if(product.getImg_exp3() != null){
-//            String expImgUrl3 = "/" + product.getImg_exp3() + "?param=" + timestamp;
-//            model.addAttribute("expImgUrl3", expImgUrl3);
-//        }
+
         return "/admin/edit_product";
     }
+
+
+
+    @ResponseBody
+    @GetMapping("/get-product-images/{productId}")
+    public ResponseEntity<byte[]> getProductImages(@PathVariable Long productId) {
+        try {
+            Product product = adminProductService.findById(productId).orElse(null);
+            if (product != null) {
+                List<byte[]> imageBytesList = new ArrayList<>();
+
+                // 썸네일 이미지
+                if (product.getImg_thumb() != null) {
+                    File thumbnailFile = new File(product.getImg_thumb());
+                    byte[] thumbnailBytes = FileUtils.readFileToByteArray(thumbnailFile);
+                    imageBytesList.add(thumbnailBytes);
+                }
+
+                // 상품 설명 이미지 1
+                if (product.getImg_exp1() != null) {
+                    File exp1File = new File(product.getImg_exp1());
+                    byte[] exp1Bytes = FileUtils.readFileToByteArray(exp1File);
+                    imageBytesList.add(exp1Bytes);
+                }
+
+                // 상품 설명 이미지 2
+                if (product.getImg_exp2() != null) {
+                    File exp2File = new File(product.getImg_exp2());
+                    byte[] exp2Bytes = FileUtils.readFileToByteArray(exp2File);
+                    imageBytesList.add(exp2Bytes);
+                }
+
+                // 상품 설명 이미지 3
+                if (product.getImg_exp3() != null) {
+                    File exp3File = new File(product.getImg_exp3());
+                    byte[] exp3Bytes = FileUtils.readFileToByteArray(exp3File);
+                    imageBytesList.add(exp3Bytes);
+                }
+
+                if (imageBytesList.isEmpty()) {
+                    return ResponseEntity.noContent().build();
+                }
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.IMAGE_JPEG);
+                return ResponseEntity.ok()
+                        .headers(headers)
+                        .body(imageBytesList.get(0));  // 첫 번째 이미지 데이터 반환
+            }
+
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+
+
+
 
     @PostMapping("/product/{id}/edit")
     public String editProduct(@PathVariable("id") Long id,
