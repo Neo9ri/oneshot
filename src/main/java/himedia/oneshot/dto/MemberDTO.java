0810a9f5d0065305e91445131f46fbdaa38b4e6c
leehyeonho20230530 <away303030@gmail.com>
2023-06-21 12:07:01 +0900

    회원가입 컨트롤러 수정

--- src/main/java/himedia/oneshot/dto/MemberDTO.java
+++ src/main/java/himedia/oneshot/dto/MemberDTO.java
@@ -16,7 +16,8 @@ public class MemberDTO {
     private String email;           //이메일
     private String name;            //이름
     private String phoneNumber;    //전화번호
-    private String idCardNumber;  //주민등록번호
+    private String idCardNumber;   //주민등록번호 앞자리
+    private String idCardNumber2;  //주민등록번호 뒷자리
     private String address;         //주소
     private String gender;          //성별
     private String authority;       //회원상태
