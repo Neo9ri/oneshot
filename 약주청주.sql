0db90782822722a3880cd56be639eec2ac6ec672
leehyeonho20230530 <away303030@gmail.com>
2023-06-23 14:25:46 +0900

    약주 청주 이미지 및 sql

---
+++ 약주청주.sql
@@ -0,0 +1,26 @@
+# 약주,  청주
+INSERT INTO product
+(name, quantity,type_local, type_kind, creator, alcohol, volume, price, img_thumb, img_exp1, img_exp2)
+values
+('산사춘', 1,'서울, 경기, 인천권', '약주/청주', '배상면주가', 12, 375, 4300, 'img/product/thumbnail/산사춘.jpg', 'img/product/explanation/산사춘_exp01.jpg', NULL),
+('산삼주', 1,'전북, 전남, 경북, 경남', '약주/청주', '백운주', 13, 375, 3500, 'img/product/thumbnail/산삼주.jpg', 'img/product/explanation/산삼주_exp01.jpg', NULL),
+('설련주', 1,'전북, 전남, 경북, 경남', '약주/청주', '석전주가', 16, 750, 35000, 'img/product/thumbnail/설련주.jpg', 'img/product/explanation/설련주_exp01.jpg', NULL),
+('청명주', 1,'전북, 전남, 경북, 경남', '약주/청주', '한영석', 13.8, 375, 29500, 'img/product/thumbnail/청명주.jpg', 'img/product/explanation/청명주_exp01.jpg', NULL),
+('백련맑은술', 1,'충북, 충남, 제주도', '약주/청주', '신평양조', 12, 375, 9000, 'img/product/thumbnail/백련맑은술.jpg', 'img/product/explanation/백련맑은술_exp01.jpg', NULL),
+('우담', 1,'전북, 전남, 경북, 경남', '약주/청주', '참주가', 13, 800, 4500, 'img/product/thumbnail/우담.jpg', 'img/product/explanation/우담_exp01.jpg', NULL),
+('우담청주', 1,'전북, 전남, 경북, 경남', '약주/청주', '참주가', 13, 700, 4200, 'img/product/thumbnail/우담청주.jpg', 'img/product/explanation/우담청주_exp01.jpg', NULL),
+('한산소곡주', 1,'충북, 충남, 제주도', '약주/청주', '우희열명인', 18, 1800, 39000, 'img/product/thumbnail/한산소곡주.jpg', 'img/product/explanation/한산소곡주_exp01.jpg', NULL),
+('이상헌약주', 1,'충북, 충남, 제주도', '약주/청주', '이가수불', 18, 500, 45000, 'img/product/thumbnail/이상헌약주.jpg', 'img/product/explanation/이상헌약주_exp01.jpg', NULL),
+('양파와인', 1,'전북, 전남, 경북, 경남', '약주/청주', '우포의아침', 12, 500, 10000, 'img/product/thumbnail/양파와인.jpg', 'img/product/explanation/양파와인_exp01.jpg', NULL),
+('맑은내일유자', 1,'전북, 전남, 경북, 경남', '약주/청주', '우포의아침', 7, 735, 18000, 'img/product/thumbnail/맑은내일유자.jpg', 'img/product/explanation/맑은내일유자_exp01.jpg', NULL),
+('하타', 1,'충북, 충남, 제주도', '약주/청주', '신탄진주조', 16, 500, 20000, 'img/product/thumbnail/하타.jpg', 'img/product/explanation/하타_exp01.jpg', NULL),
+('대덕주', 1,'충북, 충남, 제주도', '약주/청주', '신탄진주조', 16, 750, 20000, 'img/product/thumbnail/대덕주.jpg', 'img/product/explanation/대덕주_exp01.jpg', NULL),
+('미나리싱싱주', 1,'충북, 충남, 제주도', '약주/청주', '조은술세종', 14.5, 300, 5000, 'img/product/thumbnail/미나리싱싱주.jpg', 'img/product/explanation/미나리싱싱주_exp01.jpg', NULL),
+('명인솔송주', 1,'전북, 전남, 경북, 경남', '약주/청주', '명인솔송주', 13, 700, 18000, 'img/product/thumbnail/명인솔송주.jpg', 'img/product/explanation/명인솔송주_exp01.jpg', NULL),
+('조선주조사세트', 1,'전북, 전남, 경북, 경남', '약주/청주', '우호의아침', 14, 700, 15000, 'img/product/thumbnail/조선주조사세트.jpg', 'img/product/explanation/조선주조사세트_exp01.jpg', NULL),
+('달빛약주세트', 1,'전북, 전남, 경북, 경남', '약주/청주', '김포금쌀탁주영농조합', 13, 375, 35900, 'img/product/thumbnail/달빛약주세트.jpg', 'img/product/explanation/달빛약주세트_exp01.jpg', NULL),
+('한청', 1,'강원, 세종권', '약주/청주', '철원오대쌀청주', 15, 700, 20000, 'img/product/thumbnail/한청.jpg', 'img/product/explanation/한청_exp01.jpg', NULL),
+('운암1945', 1,'전북, 전남, 경북, 경남', '약주/청주', '우포의 아침', 12, 750, 3700, 'img/product/thumbnail/운암1945.jpg', 'img/product/explanation/운암1945_exp01.jpg', NULL),
+('문삼이공약주', 1,'강원, 세종권', '약주/청주', '마마스팜', 16, 500, 27000, 'img/product/thumbnail/문삼이공약주.jpg', 'img/product/explanation/문삼이공약주.jpg', NULL),
+('감자술', 1,'강원, 세종권', '약주/청주', '오대서주양조', 13, 400, 55000, 'img/product/thumbnail/감자술.jpg', 'img/product/explanation/감자술.jpg', NULL),
+
